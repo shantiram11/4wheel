f6f22fd8412333a854dc3dc009ff08a8acd77afc
shantiram11 <shantiramtiwari0852@gmail.com>
2021-12-08 16:52:27 +0545

    dashboard layout completed

---
+++ resources/views/_partials/_dashboard/_head.blade.php
@@ -0,0 +1,25 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Blank</title>
+
+    <!-- Prevent the demo from appearing in search engines -->
+    <meta name="robots" content="noindex">
+
+    <!-- Perfect Scrollbar -->
+    <link type="text/css" href="{{ asset('assets/vendor/perfect-scrollbar.css') }}" rel="stylesheet">
+
+    <!-- App CSS -->
+    <link type="text/css" href="{{ asset('assets/css/app.css') }}" rel="stylesheet">
+    {{-- <link type="text/css" href="{{ asset('assets/css/app.rtl.css') }}" rel="stylesheet"> --}}
+
+    <!-- Material Design Icons -->
+    <link type="text/css" href="{{ asset('assets/css/vendor-material-icons.css') }}" rel="stylesheet">
+    {{-- <link type="text/css" href="{{ asset('assets/css/vendor-material-icons.rtl.css') }}" rel="stylesheet"> --}}
+
+    <!-- Font Awesome FREE Icons -->
+    <link type="text/css" href="{{ asset('assets/css/vendor-fontawesome-free.css') }}" rel="stylesheet">
+    {{-- <link type="text/css" href="{{ asset('assets/css/vendor-fontawesome-free.rtl.css') }}" rel="stylesheet"> --}}
+
+</head>
---
+++ resources/views/_partials/_dashboard/_sidebar.blade.php
@@ -0,0 +1,80 @@
+<div class="mdk-drawer  js-mdk-drawer" id="default-drawer" data-align="start">
+    <div class="mdk-drawer__content">
+        <div class="sidebar sidebar-light sidebar-left sidebar-p-t" data-perfect-scrollbar>
+            <ul class="sidebar-menu">
+                <li class="sidebar-menu-item">
+                    <a class="sidebar-menu-button" data-toggle="collapse" href="#dashboards_menu">
+                        <i class="sidebar-menu-icon sidebar-menu-icon--left material-icons">dvr</i>
+                        <span class="sidebar-menu-text">Dashboards</span>
+                        <span class="ml-auto sidebar-menu-toggle-icon"></span>
+                    </a>
+                    <ul class="sidebar-submenu collapse" id="dashboards_menu">
+                        <li class="sidebar-menu-item">
+                            <a class="sidebar-menu-button" href="index.html">
+                                <span class="sidebar-menu-text">Default</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-menu-item">
+                            <a class="sidebar-menu-button" href="analytics.html">
+                                <span class="sidebar-menu-text">Analytics</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-menu-item">
+                            <a class="sidebar-menu-button" href="staff.html">
+                                <span class="sidebar-menu-text">Staff</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-menu-item">
+                            <a class="sidebar-menu-button" href="ecommerce.html">
+                                <span class="sidebar-menu-text">E-commerce</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-menu-item">
+                            <a class="sidebar-menu-button" href="dashboard-quick-access.html">
+                                <span class="sidebar-menu-text">Quick Access</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+
+            <ul class="sidebar-menu">
+                <li class="sidebar-menu-item">
+                    <a class="sidebar-menu-button" data-toggle="collapse" href="#dashboards_menu">
+                        <i class="sidebar-menu-icon sidebar-menu-icon--left material-icons">dvr</i>
+                        <span class="sidebar-menu-text">Dashboards</span>
+                        <span class="ml-auto sidebar-menu-toggle-icon"></span>
+                    </a>
+                    <ul class="sidebar-submenu collapse" id="dashboards_menu">
+                        <li class="sidebar-menu-item">
+                            <a class="sidebar-menu-button" href="index.html">
+                                <span class="sidebar-menu-text">Default</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-menu-item">
+                            <a class="sidebar-menu-button" href="analytics.html">
+                                <span class="sidebar-menu-text">Analytics</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-menu-item">
+                            <a class="sidebar-menu-button" href="staff.html">
+                                <span class="sidebar-menu-text">Staff</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-menu-item">
+                            <a class="sidebar-menu-button" href="ecommerce.html">
+                                <span class="sidebar-menu-text">E-commerce</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-menu-item">
+                            <a class="sidebar-menu-button" href="dashboard-quick-access.html">
+                                <span class="sidebar-menu-text">Quick Access</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+
+        </div>
+    </div>
+</div>
