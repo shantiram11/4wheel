0e7f751cfec3f220166873f244babe54e5211566
shantiram11 <shantiramtiwari0852@gmail.com>
2022-01-06 23:45:44 +0545

    blablabla

--- push.sh
+++ push.sh
@@ -5,8 +5,10 @@ read commitMessage
 
 git commit -m "$commitMessage"
 
-echo 'Enter the name of the branch:'
+echo 'Enter the name of the branch :'
 read branch
+
+git pull origin $branch
 
 git push origin $branch
 
