97ab7d41f93f85f1cb65ec84a9fd7bf4509b4deb
shantiram11 <shantiramtiwari0852@gmail.com>
2022-01-19 17:13:48 +0545

    action

--- resources/views/dashboard/users/_action.blade.php
+++ resources/views/dashboard/users/_action.blade.php
@@ -1,9 +1,9 @@
 @can('view',\App\Models\User::class)
-    <a href="{{ route('users.show', $r->id) }}" class="btn btn-sm btn-icon btn-light-facebook" title="{{ __('Show') }}"><i class="fas fa-eye"></i></a>
+    <a href="{{ route('user.show', $r->id) }}" class="btn btn-sm btn-icon btn-light-facebook" title="{{ __('Show') }}"><i class="fas fa-eye"></i></a>
 @endcan
 
 @can('update',\App\Models\User::class)
-    <a href="{{ route('users.edit', $r->id) }}" class="btn btn-icon btn-light-primary btn-sm ml-1p" title="{{ __('Update') }}"><i class="fas fa-pencil-alt"></i></a>
+    <a href="{{ route('user.edit', $r->id) }}" class="btn btn-icon btn-light-primary btn-sm ml-1p" title="{{ __('Update') }}"><i class="fas fa-pencil-alt"></i></a>
 @endcan
 
 @can('destroy',\App\Models\User::class)
