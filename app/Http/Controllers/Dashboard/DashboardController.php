378dd9ec142e697406c059843fe9d902d50435c9
Saroj Shrestha <thesarojstha@gmail.com>
2022-06-08 16:49:53 +0545

    chart js updated

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Controller;
 use App\Models\Booking;
+use App\Models\Category;
 use App\Models\User;
 use App\Models\Vehicle;
 use Illuminate\Http\Request;
@@ -18,19 +19,21 @@ class DashboardController extends Controller
 
     public function index()
     {
-        $vehicles = \DB::table('vehicles as s')
-            ->join('categories as c','c.id','s.category_id')
-            ->select('s.id as vehicle_id','c.name')
+        $vehicles = \DB::table('vehicles as v')
+            ->join('categories as c','c.id','v.category_id')
+            ->select('v.id as vehicle_id','c.name','c.id as category_id')
             ->get()
             ->groupBy('name');
 
-        $categories = $vehicles->keys()->toArray();
-        $vehicles_count_arr = $vehicles->map(function($item){
-            return $item->count();
+        $categories = Category::withCount('vehicles')->get()->groupBy('name');
+
+        $vehicleCountByCategories = $categories->map(function($item){
+            return $item->sum('vehicles_count');
         })->toArray();
+
         $total_users = count(User::all());
         $total_vehicles = count(Vehicle::all());
         $total_bookings = count(Booking::all());
-        return view('dashboard.index',compact('total_users','total_bookings','total_vehicles','categories','vehicles_count_arr'));
+        return view('dashboard.index',compact('total_users','total_bookings','total_vehicles','vehicleCountByCategories'));
     }
 }
