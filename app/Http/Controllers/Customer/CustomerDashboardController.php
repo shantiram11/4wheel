5f49d10dc7332298580469f7ce36b254a06caa77
Shantiram <Shantiramtiwari0852@gmail.com>
2022-06-09 16:10:44 +0545

    updated

--- app/Http/Controllers/Customer/CustomerDashboardController.php
+++ app/Http/Controllers/Customer/CustomerDashboardController.php
@@ -10,17 +10,24 @@ class CustomerDashboardController extends Controller
     public function index()
     {
         $user = auth()->user();
-        $total_bookings = $user->booking()->count();
+        $total_bookings = \DB::table('bookings as b')
+            ->join('vehicles as v', 'v.id', 'b.vehicle_id')
+         ->join('users as u', 'u.id', 'v.owner_id')
+            ->select(
+                'v.id',
+            )
+            ->where('u.id',$user->id)
+            ->count();
         $total_vehicles = $user->vehicles()->count();
         $total_earning = \DB::table('bookings as b')
             ->join('vehicles as v', 'v.id', 'b.vehicle_id')
 //            ->join('users as u', 'u.id', 'v.owner_id')
             ->select(
                 'b.total_cost',
-            'v.*',
             )
         ->where('v.owner_id',$user->id)
             ->get();
+        $total_earning = $total_earning->sum('total_cost');
         return view('customer_dashboard.index',compact('total_bookings','total_vehicles','total_earning'));
     }
 }
--- resources/views/customer_dashboard/index.blade.php
+++ resources/views/customer_dashboard/index.blade.php
@@ -17,7 +17,7 @@
                             <div class="ks-dash-box">
                                 <div class="d-flex justify-content-space-between" style="justify-content: space-between;">
                                     <div>
-                                        <i class="fas fa-users text-success" style="font-size:100px"></i>
+                                        <i class="fas fa-toolbox text-success" style="font-size:100px"></i>
                                     </div>
                                     <div class="right-col">
                                         <strong class="stat-title">Bookings</strong>
@@ -45,15 +45,13 @@
                             <div class="ks-dash-box">
                                 <div class="d-flex justify-content-space-between">
                                     <div>
-                                        <i class="fas fa-car-side text-danger" style="font-size: 100px"></i>
+                                        <i class="fas fa-comment-dollar text-info" style="font-size: 100px"></i>
                                     </div>
                                     <div class="right-col">
                                         <strong class="stat-title">Total Earnings</strong>
-                                        @dd($total_earning)
                                         <p class="stat-count">{{$total_earning}}</p>
                                     </div>
                                 </div>
-
                             </div>
                         </div>
                     </div>
